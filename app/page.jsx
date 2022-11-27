62fb69abef3dde389b3b3dfe309fa285d9beae6d
Kamal Singh <kamalsingh200238@gmail.com>
2022-11-27 14:51:39 +0530

    added logo svg

--- app/page.jsx
+++ app/page.jsx
@@ -1,9 +1,28 @@
 export default function Home() {
   return (
     <main>
-      <section>
-        <div className="bg-rose-400">Howdy</div>
-      </section>
+      <nav>
+        {/* Logo */}
+        <div>
+          <svg
+            width="100"
+            height="100"
+            viewBox="0 0 100 100"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M9.69873 26.732L50 3.4641L90.3013 26.732V73.268L50 96.5359L9.69873 73.268V26.732Z"
+              stroke="black"
+              stroke-width="6"
+            />
+            <path
+              d="M37.201 67V32.0909H43.5249V48.1307H43.951L57.5703 32.0909H65.2919L51.7919 47.7557L65.4112 67H57.809L47.3942 52.0341L43.5249 56.6023V67H37.201Z"
+              fill="black"
+            />
+          </svg>
+        </div>
+      </nav>
     </main>
   );
 }
