add540ac763e73403578fc6d4c8e6490eb7e6a5b
Kamal Singh <kamalsingh200238@gmail.com>
2022-11-29 01:22:38 +0530

    changed bacground color to primary color

--- app/page.jsx
+++ app/page.jsx
@@ -2,7 +2,7 @@ import Navbar from "../components/Navbar";
 
 export default function Home() {
   return (
-    <main className="relative overflow-x-hidden min-h-screen">
+    <main className="relative overflow-x-hidden min-h-screen bg-primary">
       <Navbar />
     </main>
   );
