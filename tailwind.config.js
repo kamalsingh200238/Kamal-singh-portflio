3857f2dfc003b445bb507f39321be5fcae2248ad
Kamal Singh <kamalsingh200238@gmail.com>
2022-11-29 01:11:38 +0530

    added secondary color in the tailwind color palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,11 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        secondary: "#60FFD9",
+      },
+    },
   },
   plugins: [],
 };
