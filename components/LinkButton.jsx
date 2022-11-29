5be3a1ca15178e39da38ee471831c2e9eacb4e8f
Kamal Singh <kamalsingh200238@gmail.com>
2022-11-29 23:14:22 +0530

    sorted tailwind classes

--- components/LinkButton.jsx
+++ components/LinkButton.jsx
@@ -1,10 +1,10 @@
 import Link from "next/link";
 
-export default function LinkButton({displayText, link}) {
+export default function LinkButton({ displayText, link }) {
   return (
     <Link
       href={link}
-      className="relative rounded-sm border border-secondary px-6 py-2 text-secondary transition-all duration-200 ease-in-out before:absolute  before:left-0 before:bottom-0 before:z-10 before:h-0 before:w-0 before:border-l-2 before:border-b-2 before:border-rose-100 before:transition-all before:duration-200 before:ease-in-out after:absolute after:right-0 after:top-0 after:z-10 after:h-0 after:w-0 after:border-r-2 after:border-t-2 after:border-rose-100 after:transition-all after:duration-200 after:ease-in-out hover:text-rose-100 before:hover:h-full before:hover:w-full after:hover:h-full after:hover:w-full max-md:mt-8 max-md:px-10"
+      className="relative rounded-sm border border-secondary px-6 py-2 text-secondary transition-all duration-200 ease-in-out before:absolute before:left-0 before:bottom-0 before:z-10 before:h-0 before:w-0 before:border-l-2 before:border-b-2 before:border-rose-100 before:transition-all before:duration-200 before:ease-in-out after:absolute after:right-0 after:top-0 after:z-10 after:h-0 after:w-0 after:border-r-2 after:border-t-2 after:border-rose-100 after:transition-all after:duration-200 after:ease-in-out hover:text-rose-100 before:hover:h-full before:hover:w-full after:hover:h-full after:hover:w-full max-md:mt-8 max-md:px-10"
     >
       <div>{displayText}</div>
     </Link>
