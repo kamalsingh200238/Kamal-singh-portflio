e77d900956d7e47b98348ea7396ccc546f0aa257
Kamal Singh <kamalsingh200238@gmail.com>
2022-12-06 17:22:47 +0530

    refactored navbar links

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,27 +1,8 @@
 "use client";
 
-import Link from "next/link";
 import { useState } from "react";
-import LinkButton from "./LinkButton";
+import NavbarLinks from "./NavbarLinks";
 import NavbarMainIcon from "./NavbarMainIcon";
-
-const navbarLinks = [
-  {
-    id: "01",
-    link: "/",
-    displayName: "About",
-  },
-  {
-    id: "02",
-    link: "/",
-    displayName: "Work",
-  },
-  {
-    id: "03",
-    link: "/",
-    displayName: "Contact",
-  },
-];
 
 export default function Navbar() {
   // state to track navbar visibility
@@ -37,17 +18,15 @@ export default function Navbar() {
         <button onClick={() => setIsOpen(!isOpen)}>
           <div className="aspect-square h-10">
             <svg
-              className={`${
-                isOpen ? "rotate-180" : ""
-              } h-full w-full stroke-secondary transition-all duration-200 ease-in-out`}
+              className={`${isOpen ? "rotate-180" : ""
+                } h-full w-full stroke-secondary transition-all duration-200 ease-in-out`}
               viewBox="0 0 148 139"
               fill="none"
               xmlns="http://www.w3.org/2000/svg"
             >
               <rect
-                className={`${
-                  isOpen ? "rotate-45" : ""
-                } origin-center fill-secondary transition-all duration-200 ease-in-out`}
+                className={`${isOpen ? "rotate-45" : ""
+                  } origin-center fill-secondary transition-all duration-200 ease-in-out`}
                 x="18"
                 y={isOpen ? "62" : "27"} // for better animations
                 width="112"
@@ -55,9 +34,8 @@ export default function Navbar() {
                 rx="5"
               />
               <rect
-                className={`${
-                  isOpen ? "opacity-0" : ""
-                } fill-secondary transition-all duration-200 ease-in-out`}
+                className={`${isOpen ? "opacity-0" : ""
+                  } fill-secondary transition-all duration-200 ease-in-out`}
                 x="18"
                 y="62"
                 width="112"
@@ -65,9 +43,8 @@ export default function Navbar() {
                 rx="5"
               />
               <rect
-                className={`${
-                  isOpen ? "-rotate-45" : ""
-                } origin-center fill-secondary transition-all duration-200 ease-in-out`}
+                className={`${isOpen ? "-rotate-45" : ""
+                  } origin-center fill-secondary transition-all duration-200 ease-in-out`}
                 x="18"
                 y={isOpen ? "62" : "97"} // for better animations
                 width="112"
@@ -80,31 +57,7 @@ export default function Navbar() {
       </div>
 
       {/* navbar links */}
-      <aside
-        className={`${
-          isOpen ? "" : "max-md:translate-x-full"
-        } grid place-items-center transition-all duration-200 ease-in-out max-md:fixed max-md:inset-y-0 max-md:right-0 max-md:w-2/3 max-md:bg-primary-800 `}
-      >
-        <nav className="flex items-center justify-center gap-6 text-sm max-md:flex-col lg:gap-8">
-          <ol className="flex items-center justify-center gap-6 max-md:flex-col lg:gap-8">
-            {navbarLinks.map((link, index) => (
-              <li key={link.displayName}>
-                <Link href={link.link} className="group block">
-                  <div className="relative flex items-center justify-center py-2 after:absolute after:inset-y-0 after:left-0 after:h-full after:w-0 after:border-b-2 after:transition-all after:duration-200 after:ease-in-out hover:after:w-full max-md:flex-col md:gap-2">
-                    <span className="font-fira text-secondary">
-                      {String("0" + (index + 1)).slice(-2)}.
-                    </span>
-                    <span className="text-primary-300 transition-all duration-200 ease-in-out group-hover:text-secondary">
-                      {link.displayName}
-                    </span>
-                  </div>
-                </Link>
-              </li>
-            ))}
-          </ol>
-          <LinkButton link={"/"} displayText={"Resume"} />
-        </nav>
-      </aside>
+      <NavbarLinks isOpen={isOpen} />
     </nav>
   );
 }
---
+++ components/NavbarLinks.jsx
@@ -0,0 +1,46 @@
+import Link from "next/link";
+import LinkButton from "./LinkButton";
+
+const navbarLinks = [
+  {
+    link: "/",
+    displayName: "About",
+  },
+  {
+    link: "/",
+    displayName: "Work",
+  },
+  {
+    link: "/",
+    displayName: "Contact",
+  },
+];
+
+export default function NavbarLinks({ isOpen }) {
+  return (
+    <aside
+      className={`${isOpen ? "" : "max-md:translate-x-full"
+        } grid place-items-center transition-all duration-200 ease-in-out max-md:fixed max-md:inset-y-0 max-md:right-0 max-md:w-2/3 max-md:bg-primary-800 `}
+    >
+      <nav className="flex items-center justify-center gap-6 text-sm max-md:flex-col lg:gap-8">
+        <ol className="flex items-center justify-center gap-6 max-md:flex-col lg:gap-8">
+          {navbarLinks.map((link, index) => (
+            <li key={link.displayName}>
+              <Link href={link.link} className="group block">
+                <div className="relative flex items-center justify-center py-2 after:absolute after:inset-y-0 after:left-0 after:h-full after:w-0 after:border-b-2 after:transition-all after:duration-200 after:ease-in-out hover:after:w-full max-md:flex-col md:gap-2">
+                  <span className="font-fira text-secondary">
+                    {String("0" + (index + 1)).slice(-2)}.
+                  </span>
+                  <span className="text-primary-300 transition-all duration-200 ease-in-out group-hover:text-secondary">
+                    {link.displayName}
+                  </span>
+                </div>
+              </Link>
+            </li>
+          ))}
+        </ol>
+        <LinkButton link={"/"} displayText={"Resume"} />
+      </nav>
+    </aside>
+  )
+}
