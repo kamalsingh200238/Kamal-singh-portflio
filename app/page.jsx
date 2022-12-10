b06e171bdad123e63c2e33c71c1d8952c4364f43
Kamal Singh <kamalsingh200238@gmail.com>
2022-12-10 17:19:04 +0530

    did a lot of things -made the main section -made it responsive

--- app/page.jsx
+++ app/page.jsx
@@ -1,31 +1,30 @@
+import LinkButton from "../components/LinkButton";
 import Navbar from "../components/Navbar";
 
 export default function Home() {
   return (
-    <main className="min-h-screen overflow-x-hidden bg-primary">
+    <div className="min-h-screen overflow-x-hidden bg-primary">
       <Navbar />
-      {/* <div className="mx-auto px-7 lg:container"> */}
-      {/*   <section> */}
-      {/*     <h1>Hi, my name is</h1> */}
-      {/*     <h2>Kamal Singh.</h2> */}
-      {/*     <h3>I build things for the web</h3> */}
-      {/**/}
-      {/*     <div> */}
-      {/*       <p> */}
-      {/*         I{"'"}m a self-taught Fullstack Developer specializing in web- and */}
-      {/*         mobile development. */}
-      {/*       </p> */}
-      {/*       <p> */}
-      {/*         It is my goal to build software which is pleasing to the eye and */}
-      {/*         even more pleasing to use. */}
-      {/*       </p> */}
-      {/*       <p> */}
-      {/*         I’m a software engineer specializing in building (and occasionally */}
-      {/*         designing) exceptional digital experiences */}
-      {/*       </p> */}
-      {/*     </div> */}
-      {/*   </section> */}
-      {/* </div> */}
-    </main>
+      <main className="mx-auto max-w-5xl px-7 md:px-10 lg:px-24">
+        <section className="grid min-h-[50vh] items-center">
+          <div className="flex flex-col gap-2 md:gap-4">
+            <h1 className="font-fira text-sm text-secondary">Hi, my name is</h1>
+            <h2 className="text-4xl font-bold text-primary-300 md:text-6xl lg:text-7xl">
+              Kamal Singh.
+            </h2>
+            <h2 className="text-3xl font-bold text-gray-400 md:text-5xl">
+              I build things for the web
+            </h2>
+            <div className="mt-4 max-w-md text-gray-400">
+              <p className="">
+                I{"'"}m a self-taught Front End Developer specializing in
+                developing beautifull and acessible websites.
+              </p>
+            </div>
+          </div>
+          {/* <LinkButton displayText={"Contact Me"} link={"/"}/> */}
+        </section>
+      </main>
+    </div>
   );
 }
