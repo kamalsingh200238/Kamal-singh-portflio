7e9426e59844e0aede21371c77db59795e96a8b4
Kamal Singh <kamalsingh200238@gmail.com>
2022-12-10 18:57:07 +0530

    changed to a fixed height in left and right side of fixed socials

--- components/LeftSideFixedButton.jsx
+++ components/LeftSideFixedButton.jsx
@@ -3,7 +3,7 @@ import { AiFillGithub, AiOutlineTwitter, AiFillLinkedin } from "react-icons/ai";
 
 const LeftSideFixedButton = () => {
   return (
-    <aside className="fixed bottom-0 left-[4%] h-[40vh] max-lg:hidden">
+    <aside className="fixed bottom-0 left-[4%] h-60 max-lg:hidden">
       <div className="grid h-full grid-rows-2 place-items-center">
         <div className="grid h-full gap-1 text-xl text-gray-400">
           <Link
--- components/RightSideFixedButton.jsx
+++ components/RightSideFixedButton.jsx
@@ -1,6 +1,6 @@
 const RightSideFixedButton = () => {
   return (
-    <aside className="fixed bottom-0 right-[4%] h-[57vh] max-lg:hidden">
+    <aside className="fixed bottom-0 right-[4%] h-80 max-lg:hidden">
       <div className="grid h-full grid-rows-2 place-items-center">
         <div className="vertical-text text-sm text-gray-400 hover:text-secondary cursor-pointer">
           kamalsingh200238@gmail.com
