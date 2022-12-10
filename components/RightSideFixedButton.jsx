fcf65552aaa0fe819323af8a3bcfadc73cfbb531
Kamal Singh <kamalsingh200238@gmail.com>
2022-12-10 21:56:58 +0530

    ran prettier on the whole project

--- components/RightSideFixedButton.jsx
+++ components/RightSideFixedButton.jsx
@@ -2,7 +2,7 @@ const RightSideFixedButton = () => {
   return (
     <aside className="fixed bottom-0 right-[4%] h-80 max-lg:hidden">
       <div className="grid h-full grid-rows-2 place-items-center">
-        <div className="vertical-text text-sm text-gray-400 hover:text-secondary cursor-pointer">
+        <div className="vertical-text cursor-pointer text-sm text-gray-400 hover:text-secondary">
           kamalsingh200238@gmail.com
         </div>
         <div className="h-24 w-[1px] self-end bg-gray-400" />
